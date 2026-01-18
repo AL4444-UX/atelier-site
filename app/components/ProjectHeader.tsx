28e5c4e04af77320b47337c83925d01484bdc8b5
Aidan lewertoff <aidanlewertoff@macbook-pro-de-aidan.home>
2026-01-18 12:48:46 +0100

    Nettoyage complet

--- app/components/ProjectHeader.tsx
+++
@@ -1,7 +0,0 @@
-export default function ProjectHeader({ title }: { title: string }) {
-return (
-<div style={{ marginBottom: 18 }}>
-<div style={{ fontSize: 48, lineHeight: 1, fontWeight: 700 }}>{title}</div>
-</div>
-);
-}
